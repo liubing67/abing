7cfe386bc618c153a0c48b0e6dca41e8b4edfd7b
liubing67 <724266839@qq.com>
2017-02-07 15:42:54 +0800

    圆角点击变色

--- app/src/main/java/com/tools/androidnote/Android_Note.java
+++ app/src/main/java/com/tools/androidnote/Android_Note.java
@@ -1,5 +1,7 @@
 package com.tools.androidnote;
 
+import android.view.WindowManager;
+
 import com.orhanobut.logger.Logger;
 
 /**
@@ -14,7 +16,34 @@ import com.orhanobut.logger.Logger;
 public class Android_Note {
 
 
+    //1
     //string 转double 保留两位小数
 //    Double d = Double.parseDouble("12222");
 //    Logger.d(String.format("%.2f",d));
+
+
+//2 改变屏幕的透明度
+// 0f<=bgAlpha<=1f
+//public void backgroundAlpha(float bgAlpha)
+//{
+//    WindowManager.LayoutParams lp = getWindow().getAttributes();
+//    lp.alpha = bgAlpha; //0.0-1.0
+//    getWindow().setAttributes(lp);
+//}
+
+
+//3
+//    private void full(boolean enable) {
+//        if (enable) {//隐藏状态栏
+//            WindowManager.LayoutParams lp = getWindow().getAttributes();
+//            lp.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+//            getWindow().setAttributes(lp);
+//        } else {//显示状态栏
+//            WindowManager.LayoutParams attr = getWindow().getAttributes();
+//            attr.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//            getWindow().setAttributes(attr);
+//        }
+//    }
+
+
 }
